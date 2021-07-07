a610f8ccb1c09be32c0f842a698c4b8f7a734b71
Sushanth172308 <80440341+Sushanth172308@users.noreply.github.com>
2021-07-07 10:11:13 +0200

    Add files via upload
    some changes

---
+++ imageViewer/build/imageViewer/imageViewer.py
@@ -0,0 +1,55 @@
+from tkinter import *
+from PIL import Image, ImageTk
+import os
+
+mw = Tk()
+mw.title('Image Viewer')
+mw.geometry('900x700')
+path = 'images'
+files_list = os.listdir(path)
+#print(files_list
+img_list = []
+for f in files_list:
+    if '.png' in f or '.jpg' in f:
+        img_with_path = path + '/' + f
+        img_list.append(img_with_path)
+#print(img_list)
+
+img = ImageTk.PhotoImage(Image.open('images/'+files_list[0]))
+img_label = Label(mw, image=img)
+img_label.grid(row=0, column=0, columnspan=3)
+
+img_num = 0
+def fwd_func(num):
+    global img, img_num
+    back_btn.config(state='normal')
+    img_num = num+1
+    #print(img_num, len(img_list))
+    img = ImageTk.PhotoImage(Image.open(img_list[img_num]))
+    img_label.config(image=img)
+    if len(img_list) == img_num + 1:
+        fwd_btn.config(state='disabled')
+
+def back_func(num):
+    global img, img_num
+    fwd_btn.config(state='normal')
+    img_num = num-1
+    #print(img_num, len(img_list))
+    img = ImageTk.PhotoImage(Image.open(img_list[img_num]))
+    img_label.config(image=img)
+    if img_num == 0:
+        back_btn.config(state='disabled')
+
+
+
+back_btn = Button(mw, text='<<', font=('', 20), state='disabled', command=lambda: back_func(img_num))
+back_btn.grid(row=1, column=0, padx=10, pady=20, sticky=W)
+
+exit_btn = Button(mw, text='exit', font=('', 20), command=mw.quit)
+exit_btn.grid(row=1, column=1, padx=10, pady=20)
+
+fwd_btn = Button(mw, text='>>', font=('', 20), command=lambda: fwd_func(img_num))
+fwd_btn.grid(row=1, column=2, padx=10, pady=20, sticky=E)
+
+
+mw.mainloop()
